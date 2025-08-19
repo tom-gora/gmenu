02bdb6ccd25a9aa4f48ef65c8224e4361def57d6
tom-gora <goratomasz@outlook.com>
2025-08-19 12:10:36 +0100

    split handling actions of: 1.creating menu items 2.handling result from menu

--- cmd/gmenu/main.go
+++ cmd/gmenu/main.go
@@ -13,9 +13,10 @@ func main() {
 		fmt.Printf("An error parsing the command arguments occured:\n%v\n", err)
 		os.Exit(1)
 	}
+	fmt.Println(conf.MenuConf.Result)
 
 	switch {
-	case conf.MenuConf != nil && conf.MenuConf.Result == nil:
+	case conf.MenuConf != nil && *conf.MenuConf.Result == "":
 		entries, err := menu.ReadJSONFile(conf.MenuConf.MenuConfPath)
 		if err != nil {
 			fmt.Printf("An error reading the menu configuration file '%s':\n%v\n", *conf.MenuConf.MenuConfPath, err)
@@ -23,13 +24,14 @@ func main() {
 		}
 		handleMenu(entries, conf.MenuConf.IsJoin)
 
-	case conf.MenuConf != nil && conf.MenuConf.Result != nil:
+	case conf.MenuConf != nil && *conf.MenuConf.Result != "":
 		entries, err := menu.ReadJSONFile(conf.MenuConf.MenuConfPath)
 		if err != nil {
 			fmt.Printf("An error reading the menu configuration file '%s':\n%v\n", *conf.MenuConf.MenuConfPath, err)
 			os.Exit(1)
 		}
 		handleMenuResult(*conf.MenuConf.Result, entries, conf.MenuConf.DefaultExec)
+
 	case conf.PickConf != nil:
 
 		handlePick(conf.PickConf)
